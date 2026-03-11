c79fcb596e46bc6e4822ab03652ad1f5e4c79490
Anna Panahi <ana.izadpanahi@gmail.com>
2026-03-10 22:04:16 -0400

    open links in footer section in separate tabs by limitation of WebDriver Scope

--- src/kayak/LimitScope.java
+++ src/kayak/LimitScope.java
@@ -1,6 +1,7 @@
 package kayak;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -18,14 +19,31 @@ public class LimitScope {
 		WebDriver driver = new ChromeDriver(options);
 		driver.get("https://www.kayak.com/");
 		
-		//print quantity or count of links (a tage)
+		// 1.print quantity or count of links (a tage)
 		System.out.println(driver.findElements(By.tagName("a")).size());
 		
 		/*
-		 * print quantity or count of links (a tage) in 'footer' only 
-		 * First select entire footer section
+		 * 2.print quantity or count of links (a tage) in 'footer' only 
+		 * First select footer section by limiting webDriver scope from entire webpage to footer
 		 */
 		WebElement footerDriver = driver.findElement(By.tagName("footer"));
 		System.out.println(footerDriver.findElements(By.tagName("a")).size());
+		
+		// 3.print count of links or anchor tags in the first column of footer section
+		WebElement columnDriver = footerDriver.findElement(By.xpath("//div[@class='w6G7-links']/nav[1]/ul"));
+		System.out.println(columnDriver.findElements(By.tagName("a")).size());
+		
+		// 4.click on each link on the firts column of footer and check if pages are opened by opening the links in separate tabs
+		for(int i = 0; i < columnDriver.findElements(By.tagName("a")).size(); i++){
+			
+			// create the keyboard shortcut "Ctrl + Enter" to open a link in a new tab
+			String clickOnLinkTab = Keys.chord(Keys.CONTROL, Keys.ENTER);
+			columnDriver.findElements(By.tagName("a")).get(i).sendKeys(clickOnLinkTab);
+			
+			// go to each tab and print the title
+			
+		}
+		
+		
 	}
 }
