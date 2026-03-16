538b6cdf133672cbc1c4d5fa1085d2f7acb574ea
Anna Panahi <ana.izadpanahi@gmail.com>
2026-03-16 12:52:55 -0400

    Validate selected date (actual value) comparing to its expected value

--- src/greenkart/ValidatePickedDate.java
+++ src/greenkart/ValidatePickedDate.java
@@ -1,9 +1,14 @@
+// validate expected and actual dates on calendar
 package greenkart;
+
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 
 public class ValidatePickedDate {
 
@@ -13,6 +18,7 @@ public class ValidatePickedDate {
 		String year = "2028";
 		String month = "6";
 		String day = "20";
+		String[] expectedDateList = { month, day, year };
 
 		// opens Chrome in maximized mode
 		ChromeOptions options = new ChromeOptions();
@@ -34,9 +40,19 @@ public class ValidatePickedDate {
 		driver.findElement(By.xpath("//button[text()='" + year + "']")).click();
 
 		// select desired month as provided above month in String
-		driver.findElements(By.cssSelector(".react-calendar__year-view__months__month")).get(Integer.parseInt(month)-1).click();
+		driver.findElements(By.cssSelector(".react-calendar__year-view__months__month"))
+				.get(Integer.parseInt(month) - 1).click();
 
 		// select desired day as provided above day in String
 		driver.findElement(By.xpath("//abbr[text()='" + day + "']")).click();
+
+		// validate the selected date
+		List<WebElement> actualDateList = driver.findElements(By.cssSelector(".react-date-picker__inputGroup__input"));
+
+		// now, iterate through the actual list above
+		for (int i = 0; i < actualDateList.size(); i++) {
+			// compae two strings of actual and expected dates
+			Assert.assertEquals(actualDateList.get(i).getAttribute("value"), expectedDateList[i]);
+		}
 	}
 }
