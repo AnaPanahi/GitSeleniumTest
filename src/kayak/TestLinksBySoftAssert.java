1f534341e445744b0b60e31f9519f2e58e6cacd2
Anna Panahi <ana.izadpanahi@gmail.com>
2026-03-28 09:23:11 -0400

    Delete a class

--- src/kayak/TestLinksBySoftAssert.java
+++ src/kayak/TestLinksBySoftAssert.java
@@ -1,5 +1,5 @@
 // testing all URLs in footer to see if they are broken with the status code of 400 or more than 400
-// this test is without using softAssertion concept from testNG -> It starts testing all links and as soon as find a broken link, the test will be stopped; so the remaining links won't be checked.
+// this test is using softAssertion concept from testNG -> It test all links and then report the broken ones if there are any
 package kayak;
 
 import java.io.IOException;
@@ -14,40 +14,38 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
 
-public class TestAllBrokenLinks {
+public class TestLinksBySoftAssert {
 
 	public static void main(String[] args) throws MalformedURLException, IOException, URISyntaxException {
 		// TODO Auto-generated method stub
-		
+
 		// opens Chrome in maximized mode
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--start-maximized");
 
 		WebDriver driver = new ChromeDriver(options);
 		driver.get("https://www.kayak.com/");
+
+		List<WebElement> links = driver.findElements(By.cssSelector("ul[class='qQ-X'] li a"));
+		SoftAssert sa = new SoftAssert();
 		
-		
-		List <WebElement> links = driver.findElements(By.cssSelector("ul[class='qQ-X'] li a"));
-		
-		for (WebElement link: links) {
+
+		for (WebElement link : links) {
 			// get all URLs (anchor tags) which are tied up to the links using Selenium
 			String hrefString = link.getAttribute("href");
 			System.out.println(hrefString);
-			
-			
-			
-			 // get the status code of each URL in the Network tab in inspect
+
+			// get the status code of each URL in the Network tab in inspect
 			/*
-			 * URI: Uniform Resource Identifier 
-			 * URI: is a class that has URL class in it that we need it. 
-			 * Why we need to access URL class -> to use the method of openConnection()		 
-			 */		
+			 * URI: Uniform Resource Identifier URI: is a class that has URL class in it
+			 * that we need it. Why we need to access URL class -> to use the method of
+			 * openConnection()
+			 */
 			HttpURLConnection conn = (HttpURLConnection) new URI(hrefString).toURL().openConnection();
-			conn.setRequestMethod("HEAD"); // set which type of http request method 
-			
-		
+			conn.setRequestMethod("HEAD"); // set which type of http request method
+
 			/*
 			 * check if the status code is 400 or more than 400; then the URL related to
 			 * that link is broken; if it is 200, then url is working well.
@@ -55,11 +53,7 @@ public class TestAllBrokenLinks {
 			conn.connect();
 			int responseCode = conn.getResponseCode();
 			System.out.println(responseCode);
-			
-			if (responseCode > 400) {
-				System.out.println("The link with text" + link.getText() + "is broken with status code of" + responseCode);
-				Assert.assertTrue(false);
-			}
+			sa.assertTrue(responseCode < 400, "The link with text" + link.getText() + "is broken with status code of" + responseCode);
 		}
 	}
 }
